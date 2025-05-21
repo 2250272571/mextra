856204b6d919a7d5b67cf58cc079b151b7fcaabd
mextraa <2250272571@qq.com>
2025-05-21 20:16:20 +0800

    修改文章

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1747812801771";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1747829702981";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
