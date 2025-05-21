ae567b54ceefe1214979e9b3d602a734f2799398
mextraa <2250272571@qq.com>
2025-05-21 22:38:55 +0800

    新增文章RTT

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1747829702981";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1747838312465";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
