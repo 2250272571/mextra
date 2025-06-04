892e5d8515d6c75a496a0d7938b3b23c942f4b0f
mextraa <2250272571@qq.com>
2025-06-04 20:47:40 +0800

    关闭自动播放音乐

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1749023664385";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1749041228081";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
