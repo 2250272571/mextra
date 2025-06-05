9283d236f75f9d4276d85ff16714d74ba04a90f8
mextraa <2250272571@qq.com>
2025-06-05 21:33:05 +0800

    修改文章gd32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1749041228081";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1749130351828";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
