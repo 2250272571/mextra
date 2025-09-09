f2ff610d37e27c93d841cb5056391893f87e6d49
Mextra <2250272571@qq.com>
2025-09-09 23:48:43 +0800

    完善ESP32环境配置部分

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1757432064854";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1757432876930";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
