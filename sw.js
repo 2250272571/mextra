f388a0d2344ffa5ac1109505acab1a69b40128ed
Mextra <2250272571@qq.com>
2025-09-12 18:51:11 +0800

    完善ESP32一文中的CMakeList

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1757673888397";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1757674252528";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
