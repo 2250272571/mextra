97a7fb1f5d7c42b6ea7ba69ad4d149e9e95dec3e
Mextra <2250272571@qq.com>
2025-09-28 12:21:10 +0800

    上传音乐封面

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759031484180";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759033246362";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
