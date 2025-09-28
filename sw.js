87e8ef744777793c0d54e654b355c4a410addd6d
Mextra <2250272571@qq.com>
2025-09-28 11:51:37 +0800

    修改音乐

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1758252065162";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759031484180";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
