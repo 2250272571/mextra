1a2e1d857b5eeb128e29fb7589abc3618e8af549
Mextra <2250272571@qq.com>
2026-01-11 02:33:45 +0800

    Site updated: YYYY-01-2026年1月11日 02:33:35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1768069724870";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1768070011662";const preCache = ["/images/taichi.png","/images/header.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
